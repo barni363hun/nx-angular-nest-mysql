8d5d1416eae76ca3e24d4dff12fb3cdafd0917cf
Bognár Pál <bognar.pal@outlook.hu>
2022-01-22 00:33:36 +0100

    Back-end authentication and authorization

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from './../auth/auth.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
@@ -5,25 +6,36 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessageModule } from '../message/message.module';
 import { MessageEntity } from '../message/message.entity';
+import { SessionEntity } from '../auth/session.entity';
+import { RoleEntity } from '../auth/role.entity';
+import { UserEntity } from '../auth/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [MessageEntity],
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: Number(process.env.DATABASE_PORT || '3306'),
+      username: process.env.DATABASE_USER || 'root',
+      password: process.env.DATABASE_PASSWORD || '',
+      database: process.env.DATABASE_NAME || 'myDatabase',
+      entities: [
+        MessageEntity,
+        UserEntity,
+        RoleEntity,
+        SessionEntity
+      ],
       //autoLoadEntities:true, //only in development mode
       synchronize: true,
     }),
     MessageModule,
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
-  
+  providers: [
+    AppService,
+  ],
+
 })
-export class AppModule {}
+export class AppModule { }
---
+++ apps/api/src/auth/auth.module.ts
@@ -0,0 +1,69 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LastAccessedMiddleWare } from './lastaccessed.middleware';
+import { RoleEntity } from './role.entity';
+import { RoleService } from './role.service';
+import { SessionEntity } from './session.entity';
+import { UserController } from './user.controller';
+import { UserEntity } from './user.entity';
+import { UserService } from './user.service';
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            SessionEntity,
+            RoleEntity,
+            UserEntity
+        ])
+    ],
+    providers: [
+        AuthService,
+        UserService,
+        RoleService
+    ],
+    controllers: [
+        AuthController,
+        UserController
+    ]
+})
+
+export class AuthModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(LastAccessedMiddleWare)
+            .forRoutes('*');
+    }
+
+    constructor(
+        private userService: UserService,
+        private roleService: RoleService) {
+
+        roleService.find({ where: { name: 'admin' } }).then(async roles => {
+            if (roles.length == 0) {
+                await this.createAdminRole();
+                userService.getByEmail('admin@droptable.jedlik.local').then(async usr => {
+                    if (!usr) {
+                        await this.createAdminUser();
+                    }
+                });
+            }
+        })
+    }
+
+    private async createAdminUser() {
+        const user = new UserEntity();
+        user.name = 'admin';
+        user.email = 'admin@droptable.jedlik.local';
+        user.passwordHash = UserService.hashPassword('admin');
+        user.roles = await this.roleService.find({ where: { name: 'admin' } });
+        await this.userService.insert(user);
+    }
+
+    private async createAdminRole() {
+        const role = new RoleEntity();
+        role.name = 'admin';
+        await this.roleService.create(role);
+    }
+}
