76ad3aeb3488bc002566556f9fa3ac8f506707af
barni363hun <barni.pbs@gmail.com>
2022-01-21 11:19:41 +0000

    gitpodifying + .env

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -11,11 +11,11 @@ import { MessageEntity } from '../message/message.entity';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Abc123456',
-      database: "my-app-nanm",
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [MessageEntity],
       //autoLoadEntities:true, //only in development mode
       synchronize: true,
